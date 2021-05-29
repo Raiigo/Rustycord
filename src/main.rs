d7b634e1d4b28b0a2e32d6939c1db47cb78678c2
Raiigo <nolann.juet@gmail.com>
2021-05-29 18:19:26 +0200

    Heartbeat sending works with timestamp (monothread)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::{net::TcpStream, thread, time::Duration};
+
 use reqwest::*;
 use tokio::*;
 use serde::{Serialize, Deserialize};
+use websocket::{ClientBuilder, Message, header::Authorization};
+use native_tls::*;
 
 #[derive(Deserialize)]
 struct SessionStartLimit {
@@ -25,12 +29,89 @@ impl GatewayInfos {
 #[tokio::main]
 async fn main() -> reqwest::Result<()> {
 
-    let request = Client::new()
+    let response = reqwest::Client::new()
         .request(Method::GET, "https://discord.com/api/v9/gateway/bot")
-        .header("Authorization", "Bot TOKEN")
-        .send().await?.json::<GatewayInfos>().await?;
+        .header("Authorization", "Bot token_artifact")
+        .send().await?.json::<GatewayInfos>().await;
 
-    println!("{}", request.get_url());
+    let gateway_infos = match response {
+        Ok(gateway_infos) => gateway_infos,
+        Err(e) => {
+            println!("Error while deserializing the response : {}", e);
+            panic!();
+        }
+    };
+
+    println!("{}", gateway_infos.get_url());
+
+    println!("Ok !");
+
+    let mut headers = websocket::header::Headers::new();
+    headers.set(Authorization("Bot token_artifact".to_owned()));
+
+    let mut client = ClientBuilder::new(gateway_infos.get_url()).unwrap()
+        .custom_headers(&headers)
+        .connect_secure(Some(TlsConnector::new().unwrap())).unwrap();
+
+    let mut text_message = match client.recv_message().unwrap() {
+        websocket::OwnedMessage::Text(text) => text,
+        websocket::OwnedMessage::Binary(bin) => String::new(),
+        websocket::OwnedMessage::Close(close) => String::new(),
+        websocket::OwnedMessage::Ping(ping) => String::new(),
+        websocket::OwnedMessage::Pong(pong) => String::new(),
+    };
+
+    text_message = text_message.replace("\"t\":null,\"s\":null,", "");
+    text_message.replace_range(40.., "}}");
+    text_message = text_message.replace(r#"{"op":10,"d":{"heartbeat_interval":"#, "").replace("}}", "");
+    
+    println!("{}", text_message);
+
+    let heartbeat_interval: u128 = text_message.parse::<u128>().unwrap();
+
+    thread::spawn(move || -> reqwest::Result<()> {
+        let mut delta = std::time::Instant::now();
+        let response = client.send_message(&Message::text(r#"
+        {
+            "op": 2,
+            "d": {
+              "token": "token_artifact",
+              "intents": 513,
+              "properties": {
+                "$os": "linux",
+                "$browser": "rust_lib",
+                "$device": "rust_lib"
+              },
+              "presence": {
+                "activities": [{
+                  "name": "League of Legends",
+                  "type": 1
+                }],
+                "status": "online",
+                "since": 91879201,
+                "afk": false
+              }
+            }
+          }
+        "#));
+        loop {
+            if delta.elapsed().as_millis() == heartbeat_interval {
+                println!("Sending heartbeat ...");
+                client.send_message(&Message::text("{\"op\": 11}"));
+                delta = std::time::Instant::now();
+            }
+        }
+        Ok(())
+    });
+
+    loop {}
 
     Ok(())
+}
+
+async fn send_heartbeat(client: &mut websocket::client::sync::Client<TlsStream<TcpStream>>, heartbeat_state: &mut bool) {
+    loop {
+        thread::sleep(Duration::from_secs(15));
+        client.send_message(&Message::text("{\"op\": 11}"));
+    }
 }
